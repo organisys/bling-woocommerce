cf78bbbbd98bed801a368b1b0b84c9275cdd355e
claudiosmweb <contato@claudiosmweb.com>
2013-10-14 18:49:36 -0300

    created initial bling-woocommerce.php file

---
+++ bling-woocommerce.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Bling WooCommerce
+ * Plugin URI: https://github.com/organisys/bling-woocommerce
+ * Description: O Bling é um sistema online que permite a você controlar as finanças, estoques e emitir notas fiscais de maneira rápida e descomplicada.
+ * Author: Bling
+ * Author URI: http://bling.com.br/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: wcbling
+ * Domain Path: /languages/
+ */
