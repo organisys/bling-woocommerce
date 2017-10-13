a7dc6efb5621e47e288e9388c7d17f5586e1d1e7
Shayron Aguiar <shayron.aguiar@gmail.com>
2017-10-13 18:05:11 -0300

    chore: resultate co-autor

--- bling-woocommerce.php
+++ bling-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: Bling WooCommerce
  * Plugin URI: https://github.com/organisys/bling-woocommerce
  * Description: The Bling is an online system that allows you to control the finances, inventory and issue invoices quickly and uncomplicated..
- * Author: Bling
+ * Author: Bling, Agência resultate
  * Author URI: http://bling.com.br/
  * Version: 1.0.5
  * License: GPLv2 or later
